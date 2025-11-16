7b6ea4662170b2296f71594a32234c59cd9c357b
abaia-ali <abdelali.lahiaoui@educa.madrid.org>
2025-11-16 19:59:27 +0100

    Add productos.php for displaying solidarity products

---
+++ productos.php
@@ -0,0 +1,23 @@
+<?php include 'header.php'; ?>
+
+<main>
+    <h2>Nuestros Productos Solidarios</h2>
+    <p>Comprando estos productos ayudas directamente a las familias de Gaza.</p>
+
+    <div class="producto">
+        <img src="img/pulsera1.jpg" alt="Pulsera artesanal">
+        <h3>Pulsera artesanal Palestina</h3>
+        <p>Donación: 8€</p>
+        <a class="btn" href="comprar.php?producto=Pulsera artesanal Palestina&precio=8">Comprar</a>
+    </div>
+
+    <div class="producto">
+        <img src="img/colgante1.jpg" alt="Colgante artesanal">
+        <h3>Colgante artesanal</h3>
+        <p>Donación: 12€</p>
+        <a class="btn" href="comprar.php?producto=Colgante artesanal&precio=12">Comprar</a>
+    </div>
+
+</main>
+
+<?php include 'footer.php'; ?>
