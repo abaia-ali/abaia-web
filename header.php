26f0a09928179314245c39a40e4c65b898fe13c7
abaia-ali <abdelali.lahiaoui@educa.madrid.org>
2025-11-16 20:47:30 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Abaia</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <h1>Abaia</h1>
+    <nav>
+        <a href="index.php">Inicio</a>
+        <a href="productos.php">Productos</a>
+        <a href="contacto.php">Contacto</a>
+        <a href="quienes-somos.php">Quiénes Somos</a>
+    </nav>
+</header>
+<main>
