72b928474c1297664a60c694f182487a11df1375
abaia-ali <abdelali.lahiaoui@educa.madrid.org>
2025-11-23 13:23:37 +0100

    Update header.php

--- header.php
+++ header.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ABAIA</title>
+    <title>ABAIA - Asociación Solidaria</title>
 
-    <!-- Bootstrap (solo aquí, no repetir) -->
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Tu CSS -->
@@ -26,7 +26,7 @@
     <div class="collapse navbar-collapse" id="menuNav">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item"><a class="nav-link" href="/index.php">Inicio</a></li>
-        <li class="nav-item"><a class="nav-link" href="/quienes-somos.php">Quiénes somos</a></li>
+        <li class="nav-item"><a class="nav-link" href="/quienes-somos.php">Quiénes Somos</a></li>
         <li class="nav-item"><a class="nav-link" href="/productos.php">Productos</a></li>
         <li class="nav-item"><a class="nav-link" href="/eventos.php">Eventos</a></li>
         <li class="nav-item"><a class="nav-link" href="/contacto.php">Contacto</a></li>
