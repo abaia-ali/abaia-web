d81d85d1e0cc716d590ad51cd88f4f7faace3206
abaia-ali <abdelali.lahiaoui@educa.madrid.org>
2025-11-23 13:15:31 +0100

    Update header.php

--- header.php
+++ header.php
@@ -9,27 +9,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Tu CSS -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">
-      <img src="img/logo.png" alt="Logo ABAIA" width="36" class="me-2"> ABAIA
+    <a class="navbar-brand" href="/index.php">
+      <img src="/img/logo.png" alt="Logo ABAIA" width="36" class="me-2"> ABAIA
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNav"
         aria-controls="menuNav" aria-expanded="false" aria-label="Menú">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-<li class="nav-item"><a class="nav-link" href="/index.php">Inicio</a></li>
-<li class="nav-item"><a class="nav-link" href="/quienes-somos.php">Quiénes somos</a></li>
-<li class="nav-item"><a class="nav-link" href="/productos.php">Productos</a></li>
-<li class="nav-item"><a class="nav-link" href="/eventos.php">Eventos</a></li>
-<li class="nav-item"><a class="nav-link" href="/contacto.php">Contacto</a></li>
-
-
+    <div class="collapse navbar-collapse" id="menuNav">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item"><a class="nav-link" href="/index.php">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="/quienes-somos.php">Quiénes somos</a></li>
+        <li class="nav-item"><a class="nav-link" href="/productos.php">Productos</a></li>
+        <li class="nav-item"><a class="nav-link" href="/eventos.php">Eventos</a></li>
+        <li class="nav-item"><a class="nav-link" href="/contacto.php">Contacto</a></li>
+      </ul>
+    </div>
   </div>
 </nav>
 
