7ca817b978b05587c26f820b59d8577e4fcc5a73
abaia-ali <abdelali.lahiaoui@educa.madrid.org>
2025-11-25 17:13:29 +0100

    Update header.php

--- header.php
+++ header.php
@@ -4,11 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ABAIA - Asociación Solidaria</title>
-
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Tu CSS -->
     <link rel="stylesheet" href="/style.css">
 </head>
 <body>
@@ -18,11 +14,9 @@
     <a class="navbar-brand" href="/index.php">
       <img src="/img/logo.png" alt="Logo ABAIA" width="36" class="me-2"> ABAIA
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNav"
-        aria-controls="menuNav" aria-expanded="false" aria-label="Menú">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNav">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="menuNav">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item"><a class="nav-link" href="/index.php">Inicio</a></li>
