aaa79f63ae13be5f9c2e40573c005d89e6e32dde
abaia-ali <abdelali.lahiaoui@educa.madrid.org>
2025-12-10 10:41:11 +0100

    Update productos.php

--- productos.php
+++ productos.php
@@ -29,7 +29,7 @@
                 <img src="img/llavero.jpg" class="card-img-top" alt="Llavero">
                 <div class="card-body">
                     <h5 class="card-title">Llavero solidario</h5>
-                    <p class="card-text">Donación: 5€</p>
+                    <p class="card-text">Donación: 1€</p>
                     <a href="gracias.php?producto=Llavero" class="btn btn-primary w-100">Quiero colaborar</a>
                 </div>
             </div>
